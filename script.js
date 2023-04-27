337f39b916c58ceefab6fd68521de3de3f1f4bae
Sean <SWaltrs23@gmail.com>
2023-04-27 10:07:51 -0400

    Create computer selection function and create play window

--- script.js
+++ script.js
@@ -1,16 +1,61 @@
 const selector = document.querySelector('#selection');
 const gameWindow = document.querySelector('#game-window');
 
-// Game Window
+// Computer Selection
+
+const getComputerSelection = () => {
+
+    switch(Math.floor(Math.random() * 6)) {
+        case 0: 
+        case 3: return 'rock';
+            break;
+        case 1:
+        case 4: return 'paper';
+            break;
+        case 2:
+        case 5: return 'scissors';
+    }
+};
+
+// Game Play
 
 
+// Play Window
+
+const playWindow = (user, puter) => {
+    gameWindow.textContent = "";
+    const playWin = document.createElement('div');
+    playWin.classList.add('play-window');
+
+    const userWindow = document.createElement('div');
+    userWindow.textContent = `${user}`;
+    playWin.appendChild(userWindow);
+
+    const para = document.createElement('p');
+    para.textContent = 'V';
+    playWin.appendChild(para);
+
+    const puterWindow = document.createElement('div');
+    puterWindow.textContent = `${puter}`;
+    playWin.appendChild(puterWindow);
+
+    gameWindow.appendChild(playWin);
+}
 
 // New Game Button
 
-const newGame = document.createElement('div');
-newGame.classList.add('new-game');
-newGame.textContent = "New Game";
-selector.appendChild(newGame);
+const newGameButton = () => {
+    const newGame = document.createElement('div');
+    newGame.classList.add('new-game');
+    newGame.textContent = "New Game";
+    selector.appendChild(newGame);
+    newGame.addEventListener('click', () => {
+        selector.textContent = "";
+        gameWindow.textContent = "";
+        playWindow('User', 'Puter');
+        selectors();
+    });
+}
 
 // Game buttons
 
@@ -21,6 +66,10 @@ const rockSelect = () => {
     para.textContent = "Rock";
     button.appendChild(para);
     selector.appendChild(button);
+
+    button.addEventListener('click', (e) => {
+        game(`${button.classList[0]}`, `${getComputerSelection()}`);
+    });
 }
 
 const paperSelect = () => {
@@ -30,6 +79,10 @@ const paperSelect = () => {
     para.textContent = "Paper";
     button.appendChild(para);
     selector.appendChild(button);
+
+    button.addEventListener('click', () => {
+        game(`${button.classList[0]}`, `${getComputerSelection()}`);
+    });
 }
 
 const scissorsSelect = () => {
@@ -39,6 +92,10 @@ const scissorsSelect = () => {
     para.textContent = "Scissors";
     button.appendChild(para);
     selector.appendChild(button);
+
+    button.addEventListener('click', () => {
+        game(`${button.classList[0]}`, `${getComputerSelection()}`);
+    });
 }
 
 const selectors = () => {
@@ -47,7 +104,15 @@ const selectors = () => {
     scissorsSelect();
 }
 
-newGame.addEventListener('click', () => {
-    selector.textContent = "";
-    selectors();
+/*
+const buttons = document.querySelectorAll('button');
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        alert('clicked');
+    });
 });
+*/
+
+
+
+newGameButton();
