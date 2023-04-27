c4b528d3471a27b90c10875533892fad39850adc
Sean <SWaltrs23@gmail.com>
2023-04-26 22:15:15 -0400

    Create game buttons and append to selection

---
+++ script.js
@@ -0,0 +1,36 @@
+const newGame = document.querySelector('.new-game');
+const selector = document.querySelector('#selection');
+
+const rockSelect = () => {
+    const button = document.createElement('button');
+    button.classList.add('rock')
+    const para = document.createElement('p');
+    para.textContent = "Rock";
+    button.appendChild(para);
+    selector.appendChild(button);
+}
+
+const paperSelect = () => {
+    const button = document.createElement('button');
+    button.classList.add('paper')
+    const para = document.createElement('p');
+    para.textContent = "Paper";
+    button.appendChild(para);
+    selector.appendChild(button);
+}
+
+const scissorsSelect = () => {
+    const button = document.createElement('button');
+    button.classList.add('scissors')
+    const para = document.createElement('p');
+    para.textContent = "Scissors";
+    button.appendChild(para);
+    selector.appendChild(button);
+}
+
+newGame.addEventListener('click', () => {
+    selector.textContent = "";
+    rockSelect();
+    paperSelect();
+    scissorsSelect();
+});
